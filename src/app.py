98ab1fdb381384511f6bcd75908d70ece16c9a11
valecase4 <valeriocaselli50@gmail.com>
2024-11-19 08:49:45 +0100

    Created a blueprint of the guide for the users.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 import imaplib
 from tkinter.messagebox import showerror
+from tkinter.font import Font
+from PIL import ImageFont
 
 current_user = None
 
@@ -33,8 +35,23 @@ def test(email_input, password_input):
     
 
 def create_app():
-    
     app = ctk.CTk()
+    # custom_font = Font(file="src/Roboto-Medium.ttf", size=26)
+    def show_help():
+        help_popup = ctk.CTkToplevel(app)
+        help_popup.title("Help")
+        help_popup.resizable(False, False)
+        help_popup.attributes("-topmost", True)
+        help_popup.grab_set()
+
+        ctk.CTkLabel(help_popup, text="WELCOME TO MYSCOREASSISTANT!", font=("Arial", 34)).pack()
+        ctk.CTkLabel(help_popup, text="MyScoreAssistant is a simple and intuitive tool designed to help university students\n \
+                     keep track of their exam grades effortlessly. The application automatically scans your university email\n \
+                     for specific messages containing your exam scores, extracts the relevant details, and stores them in\n \
+                     a secure database. With MyScoreAssistant, you can organize your academic progress seamlessly without\n \
+                     having to manually search for or record your grades.", font=("Arial", 20), justify='left').pack(pady=30)
+
+    custom_font = ImageFont.truetype(r"src\Roboto-Bold.ttf", size=30).font.family
 
     app.grid_rowconfigure(0, weight=1)
     app.grid_rowconfigure(1, weight=0) 
@@ -68,5 +85,8 @@ def create_app():
 
     scrape_btn = ctk.CTkButton(login_frame, text="Login", font=("Arial", 30), command=lambda : test(email_input, password_input))
     scrape_btn.grid(row=6, column=0)
+
+    help_button = ctk.CTkButton(app, text="Help", command=show_help, width=80, height=30)
+    help_button.place(relx=1.0, rely=0.0, anchor='ne', x=-10, y=10)
 
     return app
